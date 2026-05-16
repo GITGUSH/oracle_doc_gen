bc4390cfdcb97cf58e2e37cdf67baf28f3babecb
GITGUSH <gustavosfiocco@gmail.com>
2026-05-15 21:14:48 -0300

    Estrutura inicial do projeto

---
+++ config_exemple.py
@@ -0,0 +1,6 @@
+HOST     = ""    # IP ou hostname do servidor Oracle
+PORTA    = 1521
+SERVICE  = ""    # service name do banco
+USUARIO  = ""    # usuário Oracle
+SENHA    = ""    # senha do usuário
+SCHEMA   = ""    # schema alvo (geralmente igual ao usuário)
